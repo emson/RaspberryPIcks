fbaef25a9b7da1610904ef488f63fe0d210392f0
Ben Emson <ben@emson.co.uk>
2013-12-10 13:04:06 +0000

    Added post functionality

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,10 @@
+class MainController < ApplicationController
+  # before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # GET /posts
+  # GET /posts.json
+  def index
+    @posts = Post.all
+  end
+
+end

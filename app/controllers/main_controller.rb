d3bdd2abde4a1d3d778d578b7da622a5950f4860
Ben Emson <ben@emson.co.uk>
2014-06-20 21:04:46 +0100

    Added pagination gem

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
   # load_and_authorize_resource
 
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.paginate(:page => params[:page], :per_page => 30).order("created_at DESC")
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.paginate(:page => params[:page], :per_page => 30).order("created_at DESC")
   end
 
   # GET /posts/1
